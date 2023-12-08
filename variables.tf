9314f194bd69ea55327f80a5c03fb30a1f5754c4
karlagabriel <karla.gabriel@hashicorp.com>
2023-12-08 10:00:09 -0600

    feat: add terraform files

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 Bucket Name"
+  default = "kg-test-s3-bucket-for-fun"
+}
+
+variable "enable_versioning" {
+  description = "Enable S3 Versioning"
+  default = true
+}
